a2c8bb27f8c5819b96a84fa91ad832c93da74195
imamfarisi19 <edwardindo1@gmail.com>
2021-04-27 23:48:23 +0700

    try to fix error

--- 01-color-flipper/my/app.js
+++ 01-color-flipper/my/app.js
@@ -8,7 +8,7 @@ btn.addEvenListener("click", function() {
   console.log(randomNumber); 
 
   document.body.style.backgroundColor = colors[randomNumber]; 
-  color. textContent = colors[randomNumber]; 
+  color.textContent = colors[randomNumber]; 
 }); 
 
 function getRandomNumber() {
